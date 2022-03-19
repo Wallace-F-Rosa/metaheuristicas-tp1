b34b48318cf1f9abbcc0e07cf8d864d18b68bb0a
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-03-19 10:51:54 -0300

    fix description and print parameters

--- aco_custom.py
+++ aco_custom.py
@@ -1,13 +1,14 @@
 import time
 import argparse
 import tsplib95 as tsplib
-from heuristics import Grasp
+from heuristics import AntColony
+from search import local_search
 
 def main():
     parser = argparse.ArgumentParser(
         description='Programa lê uma instância da tsplib '+
-        'e gera solução inicial utilizando a heurística do vizinho mais '+
-        'próximo. Versão 1: adiciona cidades somente no final da solução.'
+        'e gera solução inicial utilizando a heurística de colônia de formigas'+
+        'combinada com a busca tabu.'
     )
     parser.add_argument(
         '--file',
@@ -39,6 +40,14 @@ def main():
         required=True,
         help='Valor máximo de iterações do ACO.'
     )
+
+    parser.add_argument(
+        '-n',
+        type=int,
+        default=10,
+        required=True,
+        help='Valor máximo de iterações sem melhora no ACO.'
+    )
     
     parser.add_argument(
         '-p',
@@ -50,7 +59,7 @@ def main():
     parser.add_argument(
         '-m',
         type=float,
-        default=1, required=True,
+        default=1, required=False,
         help='Limite para influência do feromômio. Valores entre 0 e 1.'
     )
 
@@ -59,6 +68,22 @@ def main():
         type=float,
         default=0.2, required=True,
         help='Taxa de Evaporação. Valores entre 0 e 1.'
+    )
+
+    parser.add_argument(
+        '-a',
+        type=int,
+        default=20,
+        required=True,
+        help='Número de formigas no ACO.'
+    )
+
+    parser.add_argument(
+        '-b',
+        type=int,
+        default=5,
+        required=True,
+        help='Número das k melhores formigas no ACO a serem usadas na atualização do feromônio.'
     )
     
     parser.add_argument(
@@ -71,26 +96,51 @@ def main():
     )
 
     args = parser.parse_args()
-    sol = []
-    if args.solution is not None:
-        sol = args.solution
-
     start = time.time()
     problem = tsplib.load(args.file)
     g = problem.get_graph()
 
     iter_max = 100
-    a = 0.2
+    iter_no_improv = 10
+    p = 0.3
+    pmax = 0.8
+    r = 0.02
+    k = 0
+    v = 0
+    nAnts = 20
+    nBest = 5
+
     if args.i:
         iter_max = args.i
 
-    if args.a:
-        a = args.a
+    if args.p:
+        p = args.p
 
-    heuristic = Grasp(g, args.k, args.v, a, iter_max)
+    if args.m:
+        pmax = args.m
+
+    if args.r:
+        r = args.r
+
+    if args.k:
+        k = args.k
+    
+    if args.v:
+        v = args.v
+
+    if args.a:
+        nAnts = args.a
+
+    if args.b:
+        nBest = args.b
+
+    if args.n:
+        iter_no_improv = args.n
+
+    heuristic = AntColony(g, k, v, p, pmax, r, nAnts, nBest, iter_max, iter_no_improv)
     sol, cost = heuristic.find_solution()
     if args.exec_data:
-        print(args.k, args.v, iter_max, a)
+        print(args.k, args.v, p, pmax, r, nAnts, nBest, iter_max, iter_no_improv)
         print(str(sol).replace(' ', ''), cost, '%s' % ((time.time() - start)))
     else:
         print(sol, cost)
